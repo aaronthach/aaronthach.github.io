f9d448133ece32fe6287159df927c175228531b0
aaronthach <athach@emory.edu>
2025-03-19 13:35:05 -0700

    improve font sizes and icons

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,3 @@
-// Scroll to top functionality
-const scrollUp = document.querySelector("#scroll-up");
-
-scrollUp.addEventListener("click", () => {
-    window.scrollTo({
-        top: 0,
-        left: 0,
-        behavior: "smooth",
-    });
-});
-
 // Nav hamburger menu selections
 const burger = document.querySelector("#burger-menu");
 const ul = document.querySelector("nav ul");
